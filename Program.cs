620cc3202b68ffd24e01a9da850d5c9ef9d7d3fe
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-04-10 10:32:09 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AirlineReservationConsoleSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
